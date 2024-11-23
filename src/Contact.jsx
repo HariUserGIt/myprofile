d6b9520ecb21b5a6dc5198c9ecd21dbdbade7af1
HariUserGit <thatavarthihariprasad@gmail.com>
2024-11-23 16:12:04 +0530

    Code push Test

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -5,7 +5,7 @@ const Contact = () => {
     <div>
       <h1>Contact</h1>
       <p>Email: thatavarthihariprasad@gmail.com</p>
-      <p>Phone: 9000054376</p>
+      <p>Phone: +91 9000054376</p>
       <p>LinkedIn: <a href="https://www.linkedin.com/in/hari-prasad-thatavarthi-8b12b0148?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app">Myprofile</a></p>
       <p>LinkedIn: <a href="https://github.com/HariUserGIt">MyRepository</a></p>
     </div>
